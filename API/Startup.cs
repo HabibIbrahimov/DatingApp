87736a2689d8bf96d98e112850c3333de38ad351
HabibIbrahimov <habibyi@code.edu.az>
2021-12-12 18:08:54 +0400

    created Migrations and Connection string

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,19 +18,21 @@ namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+        
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
+            
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options=>
             {
-                options.UseSqlite("Connection string");
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
             
